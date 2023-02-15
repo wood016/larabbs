b619b2b99527de7e435f20068f1144db92235639
wood016 <wood@homestead>
2023-02-15 06:46:03 -0600

    页面基础结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-start">由 <a href="#" target="_blank">tomcat</a> Design </p>
+    <p class="float-end"><a href="mailto:example@example.com">联系我们</a></p>
+  </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title', 'HOME')
+@section('content')
+  <h1>home page</h1>
+@stop
