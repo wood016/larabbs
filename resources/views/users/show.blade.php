4a9d7df465db24a615adf9396013b4cd043045ae
wood016 <wood@homestead>
2023-02-18 08:20:06 -0600

    用户话题列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -33,7 +33,13 @@
       {{-- 用户发布的内容 --}}
       <div class="card ">
         <div class="card-body">
-          这个人很懒，什么也没有写 -_-
+          <ul class="nav nav-tabs">
+            <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的话题</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Ta 的回复</a></li>
+          </ul>
+          @include('users._topics', [
+              'topics' => $user->topics()->recent()->paginate(5),
+          ])
         </div>
       </div>
 
