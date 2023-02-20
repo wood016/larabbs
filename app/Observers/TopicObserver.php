e1d9e2490536d459016aaa1a81e6d4a5b055f222
wood016 <wood@homestead>
2023-02-19 22:08:15 -0600

    6.8 SEO优化，翻译URL

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Handlers\SlugTranslateHandler;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -13,6 +14,7 @@ class TopicObserver
     {
         $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
+        $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
     }
 
 }
