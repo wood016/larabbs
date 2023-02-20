0d30fb4fc4a7de315a158f43e6f352048e757a4e
wood016 <wood@homestead>
2023-02-20 03:19:56 -0600

    7.1 回复数据生成

---
+++ database/seeders/RepliesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Reply;
+
+class RepliesTableSeeder extends Seeder
+{
+    use WithoutModelEvents;
+    public function run()
+    {
+        Reply::factory()->times(1000)->create();
+    }
+}
+
